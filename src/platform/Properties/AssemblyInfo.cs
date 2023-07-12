7c732f6d1ff970bc8f46280998b76e77680dbd73
agehlot-hi <gehlot.arvind@gmail.com>
2023-07-12 11:48:43 +0400

    updated project name from novweb to nov
    updated project name from novweb to nov

--- src/platform/Properties/AssemblyInfo.cs
+++ src/platform/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("XmCloudnovweb")]
+[assembly: AssemblyTitle("XmCloudnov")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("XmCloudnovweb")]
+[assembly: AssemblyProduct("XmCloudNov")]
 [assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
