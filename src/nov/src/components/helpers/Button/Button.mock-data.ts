fc4ec4a6819a61c8b89b4e06b57b9f4f4f2bb1d8
krushna-hztl <134057930+krushna-hztl@users.noreply.github.com>
2023-08-08 15:56:55 +0530

    NOV-199 button related  changes added as per suggestion

--- src/nov/src/components/helpers/Button/Button.mock-data.ts
+++ src/nov/src/components/helpers/Button/Button.mock-data.ts
@@ -13,33 +13,11 @@ const defaultData = {
       target: '_blank',
     },
   },
-  onClick: {},
   ref: null,
   className: '',
   iconClassName: 'icon-arrow-right',
 };
 
-const buttonDataWithFullWidthIcon = {
-  variant: 'button',
-  auto: false,
-  iconFullWidth: true,
-  disabled: false,
-  iconPosition: 'right',
-  text: 'About Us',
-  field: {
-    value: {
-      href: 'https://www.horizontaldigital.com',
-      text: 'Link with new tab screen reader text',
-      linktype: 'external',
-      target: '_blank',
-    },
-  },
-  onClick: {},
-  ref: null,
-  type: 'default',
-  className: '',
-  iconClassName: 'icon-arrow-right',
-};
 const primaryData = {
   variant: 'primary',
   auto: false,
@@ -55,28 +33,6 @@ const primaryData = {
       target: '_blank',
     },
   },
-  onClick: {},
-  ref: null,
-  type: 'default',
-  className: '',
-  iconClassName: 'icon-arrow-left',
-};
-const primaryWithWhiteTextData = {
-  variant: 'primaryWithWhiteText',
-  auto: false,
-  iconFullWidth: true,
-  disabled: false,
-  iconPosition: 'left',
-  text: 'About Us',
-  field: {
-    value: {
-      href: 'https://www.horizontaldigital.com',
-      text: 'Link with new tab screen reader text',
-      linktype: 'external',
-      target: '_blank',
-    },
-  },
-  onClick: {},
   ref: null,
   type: 'default',
   className: '',
@@ -97,14 +53,13 @@ const secondaryData = {
       target: '_blank',
     },
   },
-  onClick: {},
   ref: null,
   type: 'default',
   className: '',
   iconClassName: 'icon-arrow-left',
 };
-const secondaryWithRedArrowData = {
-  variant: 'secondaryWithRedArrow',
+const tertiaryData = {
+  variant: 'tertiary',
   auto: true,
   iconFullWidth: true,
   disabled: false,
@@ -118,14 +73,13 @@ const secondaryWithRedArrowData = {
       target: '_blank',
     },
   },
-  onClick: {},
   ref: null,
   type: 'default',
   className: '',
   iconClassName: 'icon-arrow-right',
 };
-const baseData = {
-  variant: 'base',
+const standardData = {
+  variant: 'standard',
   auto: true,
   iconFullWidth: true,
   disabled: false,
@@ -138,18 +92,9 @@ const baseData = {
       target: '_blank',
     },
   },
-  onClick: {},
   ref: null,
   type: 'default',
   className: '',
 };
 
-export {
-  defaultData,
-  buttonDataWithFullWidthIcon,
-  primaryData,
-  primaryWithWhiteTextData,
-  secondaryData,
-  secondaryWithRedArrowData,
-  baseData,
-};
+export { defaultData, primaryData, secondaryData, tertiaryData, standardData };
