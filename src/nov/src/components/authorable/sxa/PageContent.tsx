31afd17b8a033c74a4440465ba97d5d3577ccf6b
Nishant Hivarkar <nhivarkar@horizontal.com>
2023-08-17 20:55:47 +0530

    NOV-178: build fix.

--- src/nov/src/components/authorable/sxa/PageContent.tsx
+++ src/nov/src/components/authorable/sxa/PageContent.tsx
@@ -38,9 +38,11 @@ export const Default = (props: PageContentProps): JSX.Element => {
       </div>
     );
   }
-  const field = (props.fields && props.fields.Content
-    ? props.fields.Content
-    : sitecoreContext?.route?.fields?.Content) as RichTextField;
+  const field = (
+    props.fields && props.fields.Content
+      ? props.fields.Content
+      : sitecoreContext?.route?.fields?.Content
+  ) as RichTextField;
   return (
     <ComponentContent styles={props.params.styles} id={id}>
       <JssRichText field={field} />
