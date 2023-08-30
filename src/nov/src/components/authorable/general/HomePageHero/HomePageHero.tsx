7995692511c57b97625014566eabfe5dff8671ee
Bhavin Kateliya <bkateliya@horizontal.com>
2023-08-30 20:40:09 +0530

    NOV-164 FED Home Page Hero code cleanup

--- src/nov/src/components/authorable/general/HomePageHero/HomePageHero.tsx
+++ src/nov/src/components/authorable/general/HomePageHero/HomePageHero.tsx
@@ -1,13 +1,14 @@
-import React, { useRef, useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useBreakpoints } from '../../../utility/breakpoints';
 import Button from '@/components/helpers/Button/Button';
 import { Text } from '@sitecore-jss/sitecore-jss-nextjs';
 // Local
 import RichTextA11yWrapper from 'components/helpers/RichTextA11yWrapper/RichTextA11yWrapper';
 import Mp4VideoPlayer from '@/components/helpers/Mp4VideoPlayer/Mp4VideoPlayer';
-import Icon from '@/components/helpers/Icon/Icon';
+import HomePageHeroActionBox from './HomePageHeroActionBox';
 
 // Ideally, all this is from generated Typescript code from Sitecore and we're not manually defining types.
+
 interface HeroSliderItem {
   pageTitle?: {
     value?: string;
@@ -53,7 +54,7 @@ interface Fields {
         value?: string;
       };
       searchPlaceholderText: {
-        value: string;
+        value?: string;
       };
       trendingText: {
         value: string;
@@ -103,50 +104,6 @@ const HomePageHero = ({ fields }: HomePageHeroProps): JSX.Element => {
     // Clean up the interval when the component unmounts
     return () => clearInterval(interval);
   }, [HeroSlider.length]); // Empty dependency array ensures the effect runs only after initial render
-  const containerRef = useRef<HTMLDivElement | null>(null);
-  // const [scrollPosition, setScrollPosition] = useState(0);
-
-  // const handleScroll = (scrollOffset: number) => {
-  //   if (containerRef.current) {
-  //     containerRef.current.scrollTo({
-  //       left: containerRef.current.scrollLeft + scrollOffset,
-  //       behavior: 'smooth',
-  //     });
-  //     setScrollPosition(containerRef.current.scrollLeft);
-  //   }
-  // };
-  // const isPreviousActive = scrollPosition > 0;
-
-  // const isNextActive =
-  //   containerRef.current &&
-  //   scrollPosition < containerRef.current.scrollWidth - containerRef.current.clientWidth;
-  function replaceSpacesWithPlus(inputString: string) {
-    return inputString.replace(/ /g, '+');
-  }
-
-  const [isScrollablePrev, setIsScrollablePrev] = useState<boolean>(false);
-  const [isScrollableNext, setIsScrollableNext] = useState<boolean>(true); // Assuming it's initially scrollable
-
-  const handlePrevClick = () => {
-    // containerRef.current.scrollLeft = 0; // Set scroll position to 0
-    containerRef.current?.scrollTo({ left: 0 });
-  };
-
-  const handleNextClick = () => {
-    // containerRef.current.scrollLeft += 200; // Adjust scroll distance as needed
-    containerRef.current?.scrollBy({ left: 200 });
-  };
-  useEffect(() => {
-    const container = containerRef.current;
-    if (container) {
-      const handleScroll = () => {
-        setIsScrollablePrev(container.scrollLeft > 0);
-        setIsScrollableNext(container.scrollWidth > container.clientWidth + container.scrollLeft);
-      };
-
-      container.addEventListener('scroll', handleScroll);
-    }
-  }, []);
 
   if (fields === null || fields === undefined) return <></>;
 
@@ -246,73 +203,12 @@ const HomePageHero = ({ fields }: HomePageHeroProps): JSX.Element => {
             </div>
           </div>
         )}
-        <div className="md:flex w-full absolute right-0 bottom-0 h-[100px] bg-white lg:w-[816px] ">
-          <div
-            onClick={() => {
-              console.log('Search Popup');
-            }}
-            className="h-1/2 md:h-full flex items-center justify-center flex-shrink-0 relative after:absolute after:bottom-0 after:left-6 after:right-6 after:border-b after:border-b-gray after:opacity-25 md:after:border-0 md:pr-4"
-          >
-            <button className="h-[30px] w-[30px] text-xl text-red mx-2.5 basicFocus">
-              <Icon className="icon-search" />
-            </button>
-            <Text
-              className="text-base leading-6 text-gray-dark"
-              field={fields?.data?.item.searchPlaceholderText}
-              editable={false}
-              encode={false}
-            />
-          </div>
-          <div className="flex w-full h-1/2 md:h-full relative [&>button]:text-[35px] [&>button]:w-10 [&>button]:h-full [&>button]:text-red items-center overflow-hidden ">
-            <button
-              className={`outline-none ${isScrollablePrev && 'active'}`}
-              onClick={handlePrevClick}
-              disabled={!isScrollablePrev}
-            >
-              <Icon className="icon-chevron-left" />
-            </button>
-
-            <div
-              className="w-[calc(100%-108px)] h-full scrollable-content flex overflow-x-auto overflow-y-hidden no-scrollbar ml-[108px] scroll-smooth [&.>div]:items-center"
-              ref={containerRef}
-            >
-              <Text
-                tag="div"
-                className={`${
-                  isScrollablePrev && 'active'
-                } absolute left-14 [&.active]:left-9 text-red font-semibold`}
-                field={fields?.data?.item?.trendingText}
-                editable={true}
-                encode={false}
-              />
-
-              {Array.isArray(trendingSearchKeywordsList) &&
-                trendingSearchKeywordsList.map((Item, index: number) => (
-                  <div key={index} className="px-4 flex-shrink-0">
-                    <Button
-                      auto
-                      className="!font-normal !text-base basicFocus"
-                      field={{
-                        value: {
-                          href: searchPage + replaceSpacesWithPlus(Item),
-                          text: Item,
-                        },
-                      }}
-                      variant="secondary"
-                      tabIndex={0}
-                    />
-                  </div>
-                ))}
-            </div>
-            <button
-              className={`outline-none ${isScrollableNext && 'active'}`}
-              onClick={handleNextClick}
-              disabled={!isScrollableNext}
-            >
-              <Icon className="icon-chevron-right" />
-            </button>
-          </div>
-        </div>
+        <HomePageHeroActionBox
+          searchPage={searchPage}
+          trendingSearchKeywordsList={trendingSearchKeywordsList}
+          trendingText={fields?.data?.item?.trendingText}
+          searchPlaceholderText={fields?.data?.item.searchPlaceholderText}
+        />
       </div>
     </>
   );
