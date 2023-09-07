d2599875b3302c21b364b2a70dd7b556c0a2702c
Bhavin Kateliya <bkateliya@horizontal.com>
2023-09-07 16:40:31 +0530

    NOV-429 FED Content List-3x1

--- src/nov/src/components/authorable/general/ContentList3x1/ContentList3x1.tsx
+++ src/nov/src/components/authorable/general/ContentList3x1/ContentList3x1.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import { useRouter } from 'next/router';
 import { ImageField, Text } from '@sitecore-jss/sitecore-jss-nextjs';
 // Local
 import RichTextA11yWrapper from 'components/helpers/RichTextA11yWrapper/RichTextA11yWrapper';
@@ -62,13 +62,13 @@ const ContentList3x1 = ({ fields }: ContentList3x1Props): JSX.Element => {
 
   const targetItems = fields?.data?.datasource?.pages?.targetItems;
   const cardCtaText = fields?.data?.contextItem?.cardCtaText?.jsonValue?.value;
-
+  const router = useRouter();
   if (fields === null || fields === undefined) return <></>;
 
   return (
     <>
       <div className="w-full pt-[30px] md:pt-20 lg:pb-[50px]">
-        <div className="container ">
+        <div className="container">
           {fields?.data?.datasource?.headline?.jsonValue && (
             <div className="E4-content-list__title">
               <Text
@@ -81,46 +81,52 @@ const ContentList3x1 = ({ fields }: ContentList3x1Props): JSX.Element => {
           <div className="lg:flex lg:flex-wrap px-4">
             {Array.isArray(targetItems) &&
               targetItems?.map((Item: targetItems, index: number) => (
-                <>
-                  {Item?.primaryURL?.path && (
-                    <a
-                      key={index}
-                      className="w-full max-w-[352px] lg:max-w-none lg:w-1/3 mx-auto lg:mx-0 lg:px-[15px] justify-start flex flex-col hover:!no-underline"
-                      href={Item?.primaryURL?.path}
-                      target="_self"
-                    >
-                      {Item?.cardImage?.jsonValue?.value?.src && (
-                        <div
-                          className="w-full pt-[58.14%] md:pt-[59.09%] mt-[45px] mb-[30px] lg:mt-0 overflow-hidden bg-no-repeat bg-center bg-cover block"
-                          style={{
-                            backgroundImage: `url(${Item?.cardImage?.jsonValue?.value?.src})`,
-                          }}
-                        ></div>
-                      )}
-                      <div className="block">
-                        {Item?.headline.jsonValue?.value && (
-                          <Text
-                            tag="h3"
-                            field={Item?.headline?.jsonValue}
-                            className="text-black text-lg leading-24 font-semibold lg:text-base lg:font-bold  mb-4"
-                          />
-                        )}
-
-                        <RichTextA11yWrapper
-                          className="text-2xs leading-16 lg:text-sm lg:leading-24 [&>p]:text-2xs [&>p]:leading-16 [&>p]:lg:text-sm [&>p]:lg:leading-24"
-                          field={Item?.subheading?.jsonValue}
-                          characterLimit={100}
-                        />
-                        {cardCtaText && (
-                          <button className="inline-flex font-primary text-base leading-[normal] lg:text-sm lg:leading-24 justify-between font-medium lg:font-semibold text-lightBlack hover:text-gray-lighter items-center py-[5px] basicFocus">
-                            {cardCtaText}
-                            <Icon className="icon-chevron-right font-icomoon not-italic normal-case leading-none antialiased flex flex-col justify-center ml-[4px] text-base font-medium lg:font-semibold text-primary" />
-                          </button>
-                        )}
-                      </div>
-                    </a>
+                <div
+                  key={index}
+                  className="w-full max-w-[352px] lg:max-w-none lg:w-1/3 mx-auto lg:mx-0 lg:px-[15px] justify-start flex flex-col hover:!no-underline basicFocus cursor-pointer"
+                  // href={Item?.primaryURL?.path}
+                  onClick={() => router.push(`${Item?.primaryURL?.path}`)}
+                  onKeyUp={(e: React.KeyboardEvent<HTMLDivElement>) => {
+                    if (e.keyCode === 13 && Item?.primaryURL?.path) {
+                      e.stopPropagation();
+                      router.push(Item?.primaryURL?.path);
+                    }
+                  }}
+                  tabIndex={1}
+                >
+                  {Item?.cardImage?.jsonValue?.value?.src && (
+                    <div
+                      className="w-full pt-[58.14%] md:pt-[59.09%] mt-[45px] mb-[30px] lg:mt-0 overflow-hidden bg-no-repeat bg-center bg-cover block"
+                      style={{
+                        backgroundImage: `url(${Item?.cardImage?.jsonValue?.value?.src})`,
+                      }}
+                    ></div>
                   )}
-                </>
+                  <div className="block">
+                    {Item?.headline.jsonValue?.value && (
+                      <Text
+                        tag="h3"
+                        field={Item?.headline?.jsonValue}
+                        className="text-black text-lg leading-24 font-semibold lg:text-base lg:font-bold  mb-4"
+                      />
+                    )}
+
+                    <RichTextA11yWrapper
+                      className="text-2xs leading-16 lg:text-sm lg:leading-24 [&>p]:text-2xs [&>p]:leading-16 [&>p]:lg:text-sm [&>p]:lg:leading-24"
+                      field={Item?.subheading?.jsonValue}
+                      characterLimit={100}
+                    />
+                    {cardCtaText && (
+                      <button
+                        tabIndex={1}
+                        className="inline-flex font-primary text-base leading-[normal] lg:text-sm lg:leading-24 justify-between font-medium lg:font-semibold text-lightBlack hover:text-gray-lighter items-center py-[5px] basicFocus"
+                      >
+                        {cardCtaText}
+                        <Icon className="icon-chevron-right font-icomoon not-italic normal-case leading-none antialiased flex flex-col justify-center ml-[4px] text-base font-medium lg:font-semibold text-primary" />
+                      </button>
+                    )}
+                  </div>
+                </div>
               ))}
           </div>
         </div>
