445e43cc39e074920829587784791ada66539ecb
krushna-hztl <134057930+krushna-hztl@users.noreply.github.com>
2023-09-08 14:42:42 +0530

    NOV-597 brand capability list component added

---
+++ src/nov/src/components/authorable/general/BrandCapabilityList/BrandCapabilityList.mock-data.ts
@@ -0,0 +1,477 @@
+import { BrandCapabilityListProps } from './BrandCapabilityList';
+
+// Realistically this is going to have a lot more data from Sitecore or XM Cloud, but this is a good example mock data set.
+const defaultData: BrandCapabilityListProps = {
+  uid: '1ec6b05a-3bbc-4672-96f2-102379f51db3',
+  componentName: 'BrandCapabilityList',
+  dataSource: '{223D8E58-254B-4DEB-8824-F29A8C31107B}',
+  params: {
+    CacheClearingBehavior: 'Clear on publish',
+  },
+  fields: {
+    headline: {
+      value: 'Brands',
+    },
+    description: {
+      value:
+        'Explore our portfolio of trusted brands that supply our products and services to oilfield customers worldwide.',
+    },
+    image: {
+      value: {},
+    },
+    backgroundVideo: {
+      value:
+        'https://nov-web.s3.ca-central-1.amazonaws.com/Energy+Transition/Nov+Industries+Hero+Banner-1.mp4',
+    },
+    brandCapabilityList: [
+      {
+        value: {
+          href: '/Products-and-Services/Brands/AmClyde',
+          text: 'AmClyde',
+
+          url: null,
+        },
+      },
+      {
+        value: {
+          href: '/Products-and-Services/Brands/Ameron',
+          text: 'Ameron | Brands',
+
+          url: null,
+        },
+      },
+      {
+        value: {
+          href: '/Products-and-Services/Brands/Anson',
+          text: 'Anson',
+
+          url: null,
+        },
+      },
+      {
+        value: {
+          href: '/Products-and-Services/Brands/APL',
+          text: 'APL',
+
+          url: null,
+        },
+      },
+      {
+        value: {
+          href: '/Products-and-Services/Brands/APPCO',
+          text: 'APPCO',
+
+          url: null,
+        },
+      },
+      {
+        value: {
+          href: '/Products-and-Services/Brands/Artex',
+          text: 'Artex',
+
+          url: null,
+        },
+      },
+      {
+        value: {
+          href: '/Products-and-Services/Brands/ASEP',
+          text: 'ASEP',
+
+          url: null,
+        },
+      },
+      {
+        value: {
+          href: '/Products-and-Services/Brands/Axelson',
+          text: 'Axelson Surface Safety Products',
+
+          url: null,
+        },
+      },
+      {
+        value: {
+          href: '/Products-and-Services/Brands/Belco',
+          text: 'Belco',
+
+          url: null,
+        },
+      },
+      {
+        value: {
+          href: '/Products-and-Services/Brands/BLM',
+          text: 'BLM',
+
+          url: null,
+        },
+      },
+      {
+        value: {
+          href: '/Products-and-Services/Brands/Bowen',
+          text: 'Bowen',
+
+          url: null,
+        },
+      },
+      {
+        value: {
+          href: '/Products-and-Services/Brands/BRANDT',
+          text: 'BRANDT',
+
+          url: null,
+        },
+      },
+      {
+        value: {
+          href: '/Products-and-Services/Brands/Cellar-Tech',
+          text: 'Cellar Tech',
+
+          url: null,
+        },
+      },
+      {
+        value: {
+          href: '/Products-and-Services/Brands/Chemineer',
+          text: 'Chemineer',
+
+          url: null,
+        },
+      },
+      {
+        value: {
+          href: '/Products-and-Services/Brands/Containment-Solutions',
+          text: 'Containment Solutions',
+
+          url: null,
+        },
+      },
+      {
+        value: {
+          href: '/Products-and-Services/Brands/CTES',
+          text: 'CTES',
+
+          url: null,
+        },
+      },
+      {
+        value: {
+          href: '/Products-and-Services/Brands/Dreco',
+          text: 'Dreco',
+
+          url: null,
+        },
+      },
+      {
+        value: {
+          href: '/Products-and-Services/Brands/DynaWinch',
+          text: 'DynaWinch',
+
+          url: null,
+        },
+      },
+      {
+        value: {
+          href: '/Products-and-Services/Brands/Elmar',
+          text: 'Elmar',
+
+          url: null,
+        },
+      },
+      {
+        value: {
+          href: '/Products-and-Services/Brands/Enerflow',
+          text: 'Enerflow',
+
+          url: null,
+        },
+      },
+      {
+        value: {
+          href: '/Products-and-Services/Brands/Ershigs',
+          text: 'Ershigs',
+
+          url: null,
+        },
+      },
+      {
+        value: {
+          href: '/Products-and-Services/Brands/Fabricated-Plastics',
+          text: 'Fabricated Plastics',
+
+          url: null,
+        },
+      },
+      {
+        value: {
+          href: '/Products-and-Services/Brands/Fibra',
+          text: 'Fibra',
+
+          url: null,
+        },
+      },
+      {
+        value: {
+          href: '/Products-and-Services/Brands/Grant-Prideco',
+          text: 'Grant Prideco | Brands',
+
+          url: null,
+        },
+      },
+      {
+        value: {
+          href: '/Products-and-Services/Brands/Greerco',
+          text: 'Greerco',
+
+          url: null,
+        },
+      },
+      {
+        value: {
+          href: '/Products-and-Services/Brands/Guardian',
+          text: 'Guardian',
+
+          url: null,
+        },
+      },
+      {
+        value: {
+          href: '/Products-and-Services/Brands/GustoMSC',
+          text: 'GustoMSC',
+
+          url: null,
+        },
+      },
+      {
+        value: {
+          href: '/Products-and-Services/Brands/Hercules',
+          text: 'Hercules',
+
+          url: null,
+        },
+      },
+      {
+        value: {
+          href: '/Products-and-Services/Brands/Hitec',
+          text: 'Hitec',
+
+          url: null,
+        },
+      },
+      {
+        value: {
+          href: '/Products-and-Services/Brands/Hydra-Rig',
+          text: 'Hydra Rig',
+
+          url: null,
+        },
+      },
+      {
+        value: {
+          href: '/Products-and-Services/Brands/Hydralift',
+          text: 'Hydralift',
+
+          url: null,
+        },
+      },
+      {
+        value: {
+          href: '/Products-and-Services/Brands/Kenics',
+          text: 'Kenics',
+
+          url: null,
+        },
+      },
+      {
+        value: {
+          href: '/Products-and-Services/Brands/MD-Totco',
+          text: 'M/D Totco | Brands',
+
+          url: null,
+        },
+      },
+      {
+        value: {
+          href: '/Products-and-Services/Brands/MISSION',
+          text: 'MISSION ',
+
+          url: null,
+        },
+      },
+      {
+        value: {
+          href: '/Products-and-Services/Brands/MoldeCrane',
+          text: 'MoldeCrane',
+
+          url: null,
+        },
+      },
+      {
+        value: {
+          href: '/Products-and-Services/Brands/Mono-and-Moyno',
+          text: 'Mono and Moyno',
+
+          url: null,
+        },
+      },
+      {
+        value: {
+          href: '/Products-and-Services/Brands/MSI-Pipe-Protection-Technologies',
+          text: 'MSI Pipe Protection Technologies',
+
+          url: null,
+        },
+      },
+      {
+        value: {
+          href: '/Products-and-Services/Brands/Plasti-Fab',
+          text: 'Plasti Fab',
+
+          url: null,
+        },
+      },
+      {
+        value: {
+          href: '/Products-and-Services/Brands/Prochem',
+          text: 'Prochem',
+
+          url: null,
+        },
+      },
+      {
+        value: {
+          href: '/Products-and-Services/Brands/Procon',
+          text: 'Procon',
+
+          url: null,
+        },
+      },
+      {
+        value: {
+          href: '/Products-and-Services/Brands/Quality-Tubing',
+          text: 'Quality Tubing',
+
+          url: null,
+        },
+      },
+      {
+        value: {
+          href: '/Products-and-Services/Brands/ReedHycalog',
+          text: 'ReedHycalog | Brands',
+
+          url: null,
+        },
+      },
+      {
+        value: {
+          href: '/Products-and-Services/Brands/Remacut',
+          text: 'Remacut',
+
+          url: null,
+        },
+      },
+      {
+        value: {
+          href: '/Products-and-Services/Brands/Rolligon',
+          text: 'Rolligon',
+
+          url: null,
+        },
+      },
+      {
+        value: {
+          href: '/Products-and-Services/Brands/Seabox',
+          text: 'Seabox',
+
+          url: null,
+        },
+      },
+      {
+        value: {
+          href: '/Products-and-Services/Brands/Sentry',
+          text: 'Sentry',
+
+          url: null,
+        },
+      },
+      {
+        value: {
+          href: '/Products-and-Services/Brands/Shaffer',
+          text: 'Shaffer',
+
+          url: null,
+        },
+      },
+      {
+        value: {
+          href: '/Products-and-Services/Brands/Texas-Oil-Tools',
+          text: 'Texas Oil Tools',
+
+          url: null,
+        },
+      },
+      {
+        value: {
+          href: '/Products-and-Services/Brands/Tolteq',
+          text: 'Tolteq',
+
+          url: null,
+        },
+      },
+      {
+        value: {
+          href: '/Products-and-Services/Brands/Tuboscope',
+          text: 'Tuboscope | Brands',
+
+          url: null,
+        },
+      },
+      {
+        value: {
+          href: '/Products-and-Services/Brands/Varco',
+          text: 'Varco',
+
+          url: null,
+        },
+      },
+      {
+        value: {
+          href: '/Products-and-Services/Brands/Varco-BJ',
+          text: 'Varco BJ',
+
+          url: null,
+        },
+      },
+      {
+        value: {
+          href: '/Products-and-Services/Brands/Wellstream-Processing',
+          text: 'Wellstream Processing',
+          url: null,
+        },
+      },
+      {
+        value: {
+          href: '/Products-and-Services/Brands/Wilco',
+          text: 'Wilco',
+
+          url: null,
+        },
+      },
+      {
+        value: {
+          href: '/Products-and-Services/Brands/Yale',
+          text: 'Yale',
+
+          url: null,
+        },
+      },
+    ],
+    helpText: null,
+    hideSearch: false,
+  },
+};
+
+export const noData = {
+  render: {},
+  params: [],
+};
+
+export default defaultData;
