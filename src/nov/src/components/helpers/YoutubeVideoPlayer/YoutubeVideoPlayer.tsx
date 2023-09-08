735b9a762a2c205bf94896bce692594b64336858
Bhavin Kateliya <bkateliya@horizontal.com>
2023-09-08 18:51:54 +0530

    NOV-439 FED video player issue

--- src/nov/src/components/helpers/YoutubeVideoPlayer/YoutubeVideoPlayer.tsx
+++ src/nov/src/components/helpers/YoutubeVideoPlayer/YoutubeVideoPlayer.tsx
@@ -1,3 +1,5 @@
+import React, { useEffect, useState } from 'react';
+
 export interface YoutubeVideoPlayerProps {
   // fields: Field;
   field: {
@@ -19,10 +21,16 @@ const YoutubeVideoPlayer = ({ field }: YoutubeVideoPlayerProps): JSX.Element => 
   } else if (youtubeUrl.includes('youtube.com/embed/')) {
     videoId = youtubeUrl.split('/').pop();
   }
+  const [windowOrigin, setwindowOrigin] = useState('');
+
+  useEffect(() => {
+    const windowOriginValue = window.location.origin;
+    setwindowOrigin(windowOriginValue);
+  }, []);
   return (
     <div className="w-full relative pt-[56.25%]">
       <iframe
-        src={`https://www.youtube.com/embed/${videoId}`}
+        src={`https://www.youtube.com/embed/${videoId}?fs=1&modestbranding=1&origin=${windowOrigin}&rel=0&enablejsapi=1&widgetid=1`}
         className="absolute top-0 left-0 w-full h-full"
         title="YouTube video player"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
